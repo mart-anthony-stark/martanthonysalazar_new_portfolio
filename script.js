1ed79501c16bb1eb3063460abcb6d57d10dd2219
Mart Anthony Salazar <>
2021-04-02 02:56:02 +0000

    Ashazva

--- script.js
+++ script.js
@@ -10,3 +10,38 @@ setInterval(()=>{
 setInterval(()=>{
   loader.style.opacity = '0'
 }, 3500)
+
+
+// SIDEBAR REACTIVITY
+const burger = document.querySelector('.burger')
+const sidebar = document.querySelector('.sidebar')
+const main = document.querySelector('.wrapper')
+const links = document.querySelectorAll('.sidebar a')
+
+burger.addEventListener('click', function(){
+  if(burger.classList.contains('show')){
+    sidebar.style.margin = "0 0 0 100%";
+    burger.classList.remove('show')
+  }else{
+    sidebar.style.margin = "0 0 0 calc(100% - 300px)";
+    burger.classList.add('show')
+  }
+
+})
+
+main.addEventListener('click', function(){
+  if(burger.classList.contains('show')){
+    sidebar.style.margin = "0 0 0 100%";
+    burger.classList.remove('show')
+  }
+})
+
+console.log(links)
+links.forEach(link => {
+  link.addEventListener('click', function(){
+    if(burger.classList.contains('show')){
+      sidebar.style.margin = "0 0 0 100%";
+      burger.classList.remove('show')
+    }
+  })
+})
